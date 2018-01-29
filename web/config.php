f39efe112ef24e6d8cc0c616e4a86e60c495204f
Leicia_Anna <35892308+LeticiaAnna@users.noreply.github.com>
2018-01-29 19:26:13 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBbLFBhwPDng3qllnvtIRy2fD77NPQb2R8');//这里改成您的api key
-define('GJ_CODE', 'TW');//国家代码
+define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'you2php - 免翻墙看youtube');//网站名字
 define('TITLENAME', 'you2php - By:周津宇');//网站名字
 define('EN2DEKEY', 'BZsymGEtsC');//一个随机字符串，加解密KEY
